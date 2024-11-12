d430a256acb01290710bea5b753e94391af050ea
GregTses <gregtsitses@gmail.com>
2024-11-12 17:10:03 +0200

    Update w06.d01 excersises and progress.w06.d01.csv

---
+++ user/week06/exercises/day01/utility_classes/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./**/*.html"],
+  content: [],
+  theme: { extend: { colors: { primary: "#ff0000" } } },
+  plugins: [],
+};
